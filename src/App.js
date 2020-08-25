06dc400fb17a2cc8683ba326a7c4417b9d2745f1
Adrian-P0gar <pogar.adi@gmail.com>
2020-08-25 16:54:21 +0300

    route for home and list shows

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import './App.css';
-import { SerialProvider } from './SerialContext';
-import ListedShows from './components/ListedShows'
+import React from "react";
+import "./App.css";
+import { SerialProvider } from "./SerialContext";
+import ListedShows from "./components/ListedShows";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
+  return (
+    <Router>
+      <SerialProvider>
+        <div className="App">
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-	return (
-		<SerialProvider>
-			<div className="App">
-				<ListedShows />
-			</div>
-		</SerialProvider>
-	)
+          <Route path="/shows">
+            <ListedShows/>
+          </Route>
+        </div>
+      </SerialProvider>
+    </Router>
+  );
 }
 
 export default App;
-
-
