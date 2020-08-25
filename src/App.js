662dc2ff0f84e5f393a8498a8c5f35201047b681
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-08-25 22:20:09 +0300

    Simple bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import { SerialProvider } from './SerialContext';
 import ListedShows from './components/ListedShows'
+import { default as Nav } from './components/layout/navBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
 	return (
 		<SerialProvider>
 			<div className="App">
+				<Nav />
 				<ListedShows />
 			</div>
 		</SerialProvider>
