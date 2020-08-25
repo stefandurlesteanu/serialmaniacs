4552aa42e09ce55a7413ad13030d4ba1d84d4fad
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-08-25 14:53:37 +0300

    Debugged context api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './App.css';
-import { SerialProvider, SerialContext } from './SerialContext';
+import { SerialProvider } from './SerialContext';
 import ListedShows from './components/ListedShows'
 
 function App() {
-
-	const serial = useContext(SerialContext);
-
-	console.log(serial)
 
 	return (
 		<SerialProvider>
 			<div className="App">
 				<ListedShows />
-
-				{/* {serial.map((ser) => (
-					<>
-						<p>{ser.id}</p>
-						<p>{ser.name}</p>
-						<p>{ser.network.country.name}</p>
-					</>
-				))} */}
 			</div>
 		</SerialProvider>
 	)
--- src/components/ListedShows.js
+++ src/components/ListedShows.js
@@ -1,12 +1,19 @@
 import React, { useContext } from 'react';
 import { SerialContext } from '../SerialContext';
 
-export default function MyPokemons() {
-	const serial = useContext(SerialContext);
+export default function ListedShows() {
+
+	const [serial, setSerial] = useContext(SerialContext);
 
 	return (
 		<div>
-			{serial}
+			{serial.map((ser) => (
+				<div key={ser.id}>
+					<p>{ser.id}</p>
+					<p>{ser.name}</p>
+					<p>{ser.network.country.name}</p>
+				</div>
+			))}
 		</div>
 	)
 }
