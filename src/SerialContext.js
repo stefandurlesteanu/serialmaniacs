802c8017793afbbc04d0295be432a5c2e34fa7d1
stefandurlesteanu <stefan.durlesteanu@gmail.com>
2020-08-27 21:14:34 +0300

    v.0.0.3

--- src/SerialContext.js
+++ src/SerialContext.js
@@ -4,34 +4,34 @@ import { getData } from "./components/services/getData";
 export const SerialContext = createContext();
 
 export const SerialProvider = (props) => {
-  const initialUrl = "http://api.tvmaze.com/shows";
-  const [serial, setSerial] = useState([]);
-  const [loading, setLoading] = useState(true);
+	const initialUrl = "https://api.tvmaze.com/shows";
+	const [serial, setSerial] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function fetchData() {
-      let response = await getData(initialUrl);
-      function waitForElement() {
-        if (typeof response !== "undefined") {
-          setSerial(response);
-          setLoading(false);
-        } else {
-          setTimeout(waitForElement, 250);
-        }
-      }
-      waitForElement();
-    }
-    fetchData();
-  }, []);
+	useEffect(() => {
+		async function fetchData() {
+			let response = await getData(initialUrl);
+			function waitForElement() {
+				if (typeof response !== "undefined") {
+					setSerial(response);
+					setLoading(false);
+				} else {
+					setTimeout(waitForElement, 250);
+				}
+			}
+			waitForElement();
+		}
+		fetchData();
+	}, []);
 
-  return (
-    <SerialContext.Provider
-      value={{
-        loadedSerial: [serial, setSerial],
-        loadingData: [loading, setLoading],
-      }}
-    >
-      {props.children}
-    </SerialContext.Provider>
-  );
+	return (
+		<SerialContext.Provider
+			value={{
+				loadedSerial: [serial, setSerial],
+				loadingData: [loading, setLoading],
+			}}
+		>
+			{props.children}
+		</SerialContext.Provider>
+	);
 };
