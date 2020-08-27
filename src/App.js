0826ad0bf1f84b1968bd23d5a3fb1ad07787b2af
FlorinCiocirlan <florineless@gmail.com>
2020-08-27 11:09:59 +0300

    fixed link issues + imports

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from "react";
-import "./App.css";
-import { SerialProvider } from "./SerialContext";
-import ListedShows from "./components/ListedShows";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./Home";
+import React from 'react';
+import './App.css';
+import { SerialProvider } from './SerialContext';
+import ListedShows from './components/ListedShows';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import { ShowPage } from './components/ShowPage';
+import Footer from './components/layout/footer';
+import { default as Nav } from './components/layout/navBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Router>
-      <SerialProvider>
-        <div className="App">
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/shows">
-            <ListedShows />
-          </Route>
-        </div>
-      </SerialProvider>
-    </Router>
+    <SerialProvider>
+      <div className='App'>
+        <Router>
+          <div className='contentWrap'>
+            <Nav />
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/shows'>
+              <ListedShows />
+            </Route>
+            <Route path='/show/:id' component={ShowPage} />
+            <Footer />
+          </div>
+        </Router>
+      </div>
+    </SerialProvider>
   );
 }
 
--- src/components/layout/navBar.js
+++ src/components/layout/navBar.js
@@ -1,37 +1,44 @@
-import React from 'react'
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
-import brandLogo from '../layout/brandLogo.png'
+import React from 'react';
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from 'react-bootstrap';
+import brandLogo from '../layout/brandLogo.png';
 
 export default function navBar() {
-	return (
-		<Navbar className="color-nav" expand="lg">
-			<Navbar.Brand href="#home">
-				<img
-					src={brandLogo}
-					width="50"
-					height="50"
-					className="d-inline-block align-top"
-					alt="logo"
-				/>
-			</Navbar.Brand>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="mr-auto">
-					<Nav.Link href="#home">Shows</Nav.Link>
-					<Nav.Link href="#link">Genres</Nav.Link>
-					<NavDropdown title="More" id="basic-nav-dropdown">
-						<NavDropdown.Item href="#action/3.1">Best Shows</NavDropdown.Item>
-						<NavDropdown.Item href="#action/3.2">Most Wanted</NavDropdown.Item>
-						<NavDropdown.Item href="#action/3.3">ActorPedia</NavDropdown.Item>
-						<NavDropdown.Divider />
-						<NavDropdown.Item href="#action/3.4">Contact Us</NavDropdown.Item>
-					</NavDropdown>
-				</Nav>
-				<Form inline>
-					<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-					<Button variant="outline-success">Search</Button>
-				</Form>
-			</Navbar.Collapse>
-		</Navbar>
-	)
+  return (
+    <Navbar className='color-nav' expand='lg'>
+      <Navbar.Brand href='#home'>
+        <img
+          src={brandLogo}
+          width='50'
+          height='50'
+          className='d-inline-block align-top'
+          alt='logo'
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+          <Nav.Link href='/shows'>Shows</Nav.Link>
+          <Nav.Link href='#link'>Genres</Nav.Link>
+          <NavDropdown title='More' id='basic-nav-dropdown'>
+            <NavDropdown.Item href='#action/3.1'>Best Shows</NavDropdown.Item>
+            <NavDropdown.Item href='#action/3.2'>Most Wanted</NavDropdown.Item>
+            <NavDropdown.Item href='#action/3.3'>ActorPedia</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href='#action/3.4'>Contact Us</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type='text' placeholder='Search' className='mr-sm-2' />
+          <Button variant='outline-success'>Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
